2cae5c07347806d2a9409b8dd2b246bbee2864db
Myat Thu Ko <nymyatthuko@gmail.com>
2020-05-09 15:44:06 -0400

    Finished the project

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //IB means Interface Builder
+    //@IBOutlet allows to reference a UI element
+    // press control and click the UI Element and drag to the main.storybaord
+    // to show side to side use the assistant in the editor
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let dices = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //who.what = value
+        diceImageView1.image = #imageLiteral(resourceName: "DiceThree")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+        // two ways to randomzie
+        
+//        diceImageView1.image = dices[Int.random(in: 0...5)]
+//        diceImageView2.image = dices[Int.random(in: 0...5)]
+        
+        diceImageView1.image = dices.randomElement()
+        diceImageView2.image = dices.randomElement()
+    }
+    
 }
 
