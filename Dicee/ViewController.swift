208c4462a3a7d14d7c4fa81faec9e54218e6b29d
Myat Thu Ko <nymyatthuko@gmail.com>
2020-05-09 13:53:44 -0400

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Myat Thu Ko on 5/9/20.
+//  Copyright © 2020 Myat Thu Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
